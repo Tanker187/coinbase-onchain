81186a728ffc0fb0bb54e91bcebb267eb34fb0c9
Tanker187 <yesim100ya@outlook.com>
2026-09-12 18:52:09 -0400

    Fix homepage login button navigation

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,12 +1,14 @@
 'use client';
-import WalletWrapper from './WalletWrapper';
+
+import Link from 'next/link';
 
 export default function LoginButton() {
   return (
-    <WalletWrapper
-      className="min-w-[90px]"
-      text="Log in"
-      withWalletAggregator={true}
-    />
+    <Link
+      href="/login?mode=login"
+      className="inline-flex min-w-[90px] items-center justify-center rounded-lg border border-white/15 bg-white/10 px-4 py-2 text-sm font-medium text-white hover:bg-white/15"
+    >
+      Log in
+    </Link>
   );
 }
