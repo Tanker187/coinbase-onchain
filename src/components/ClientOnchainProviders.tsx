75ca9bf1378e86cfae2aff67f7e9c8481ec55f7e
Tanker187 <yesim100ya@outlook.com>
2026-09-12 21:33:15 -0400

    Fix Next.js 15 server-component dynamic provider import

---
+++ src/components/ClientOnchainProviders.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+
+const OnchainProviders = dynamic(
+  () => import('./OnchainProviders'),
+  {
+    ssr: false,
+  },
+);
+
+export default function ClientOnchainProviders({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <OnchainProviders>{children}</OnchainProviders>;
+}
