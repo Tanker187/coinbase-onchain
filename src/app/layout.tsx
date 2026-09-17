bc6779426d8f3bffc793c863e936cc4e16402cee
Tanker187 <yesim100ya@outlook.com>
2026-09-17 09:17:30 -0400

    Fix client-side provider rendering

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,8 @@
 import type { Metadata } from 'next';
-import { NEXT_PUBLIC_URL } from '../config';
-
 import './global.css';
 import '@coinbase/onchainkit/styles.css';
 import '@rainbow-me/rainbowkit/styles.css';
-import dynamic from 'next/dynamic';
-
-const OnchainProviders = dynamic(
-  () => import('src/components/OnchainProviders'),
-  {
-    ssr: false,
-  },
-);
+import OnchainProviders from 'src/components/OnchainProviders';
 
 export const viewport = {
   width: 'device-width',
